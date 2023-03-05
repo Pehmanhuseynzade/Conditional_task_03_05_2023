3e8d2bc500d2eec34c2f048f6b8b808e290c3aa0
Pehman <7m4av2m@code.edu.az>
2023-03-06 00:27:03 +0400

    Tasklarin hamisi buradadir

---
+++ Task_Conditional/Program.cs
@@ -0,0 +1,37 @@
+﻿//Task1
+
+//int birthday_date = 2005;
+
+//int result = birthday_date + 18;
+
+//if (result==2023)
+//        {
+//    Console.WriteLine("Artiq 2023-cu ildir!");
+//        }
+
+//else if (!(result>2023))
+//    {
+//    Console.WriteLine("18 yasiniz tamam olub!");
+//    }
+
+//else
+//{
+//    Console.WriteLine("18 yasiniz tamam olmayib!");
+//}
+
+//Task3
+
+//int four_digit_number = 6891;
+
+//int a = four_digit_number / 1000;
+
+//int b = four_digit_number / 100 % 10;
+
+//int c = four_digit_number / 10 % 10;
+
+//int d = four_digit_number % 10;
+
+//int result = a + b + c + d;
+
+//Console.WriteLine(result);
+
